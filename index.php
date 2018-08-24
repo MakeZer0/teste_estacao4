5469995afe6f5297deb82dae51cf051bec98846d
MakeZer0 <33382934+MakeZer0@users.noreply.github.com>
2018-08-23 23:47:02 -0300

    Ajuste CSS

--- index.php
+++ index.php
@@ -33,8 +33,10 @@
     </nav>
     <div class="image">
       <img src="img/warebg.png" class="img-fluid" alt="Responsive image">
-      <h2 id="txt_home"><span>Sistema de Controle de Produtos<span class='spacer'></span></h2>
-      <h2 id="txt_home_bot"><span>Inserir, Listar, Atualizar e Remover Produtos<span class='spacer'></span></h2>
+      <div id="home_cont">
+        <h2 id="txt_home"><span>Sistema de Controle de Produtos<span class='spacer'></span></h2>
+        <h2 id="txt_home_bot"><span>Inserir, Listar, Atualizar e Remover Produtos<span class='spacer'></span></h2>
+      </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
